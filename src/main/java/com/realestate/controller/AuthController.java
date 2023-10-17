b99246827e152e92c11373651672fdbcb7d51d23
Maciej Jankowski <jankowski.maciej0@gmail.com>
2023-10-17 14:43:00 +0200

    Registration and login methods to the new AuthService have been moved. Only method calls are left in the controller.

--- src/main/java/com/realestate/controller/AuthController.java
+++ src/main/java/com/realestate/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.realestate.dto.RegisterDto;
 import com.realestate.model.user.*;
 import com.realestate.repository.RoleRepository;
 import com.realestate.repository.UserRepository;
+import com.realestate.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,46 +28,24 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JWTGenerator jwtGenerator;
+    private final AuthService authService;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JWTGenerator jwtGenerator) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtGenerator = jwtGenerator;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterDto registerDto){
-        if (userRepository.existsByEmail(registerDto.getEmail())){
-            return new ResponseEntity<>("Email is taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        UserEmployee user = new UserEmployee();
-        user.setFirstName(registerDto.getFirstName());
-        user.setLastName(registerDto.getLastName());
-        user.setEmail(registerDto.getEmail());
-        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
-
-        Role roleAgent = roleRepository.findByName("AGENT").get();
-        user.setRoles(Collections.singletonList(roleAgent));
-
-        userRepository.save(user);
-        return new ResponseEntity<>("User registered successfully!", HttpStatus.OK);
+        String result = authService.registerUser(registerDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
 
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
-        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authenticate);
-        String token = jwtGenerator.generatedToken(authenticate);
+        String token = authService.loginUser(loginDto);
         return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
+
 
     }
 }
---
+++ src/main/java/com/realestate/service/AuthService.java
@@ -0,0 +1,63 @@
+package com.realestate.service;
+
+import com.realestate.config.JWTGenerator;
+import com.realestate.dto.LoginDto;
+import com.realestate.dto.RegisterDto;
+import com.realestate.exception.RegistrationException;
+import com.realestate.model.user.Role;
+import com.realestate.model.user.UserEmployee;
+import com.realestate.repository.RoleRepository;
+import com.realestate.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTGenerator jwtGenerator;
+
+    public AuthService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JWTGenerator jwtGenerator) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    public String registerUser(RegisterDto registerDto) {
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
+            return "Email is taken!";
+        }
+
+        UserEmployee user = new UserEmployee();
+        user.setFirstName(registerDto.getFirstName());
+        user.setLastName(registerDto.getLastName());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Role roleAgent = roleRepository.findByName("AGENT").orElseThrow(() -> new RuntimeException("Role not found"));
+        user.setRoles(Collections.singletonList(roleAgent));
+
+        userRepository.save(user);
+        return "User registered successfully!";
+    }
+
+    public String loginUser(LoginDto loginDto) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        String token = jwtGenerator.generatedToken(authenticate);
+        return token;
+    }
+
+
+}
