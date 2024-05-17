e37af80c6cff5055c5d027fb176f491ff8ed94bd
Maciej Jankowski <jankowski.maciej0@gmail.com>
2024-05-17 10:28:52 +0200

    Validation for Apartment has been updated.

--- src/main/java/com/realestate/dto/ApartmentPropertyDto.java
+++ src/main/java/com/realestate/dto/ApartmentPropertyDto.java
@@ -14,26 +14,28 @@ public class ApartmentPropertyDto {
     private Long id;
     @NotBlank(message = "Address cannot be empty")
     private String address;
-    @NotNull(message = "Price cannot be null")
+    @NotNull(message = "Price cannot be empty")
     @Positive(message = "Price must be a positive value")
     private BigDecimal price;
     private String description;
-    @NotNull(message = "Area cannot be null")
+    @NotNull(message = "Area cannot be empty")
     @Positive(message = "Area must be a positive value")
     private Double area;
-    @NotNull(message = "Number of rooms cannot be null")
+    @NotNull(message = "Number of rooms cannot be empty")
     @Positive(message = "Number of rooms must be a positive value")
     private Integer rooms;
-    @NotNull(message = "Number of bathrooms cannot be null")
+    @NotNull(message = "Number of bathrooms cannot be empty")
     @Positive(message = "Number of bathrooms must be a positive value")
     private Integer bathrooms;
     private Boolean duplexApartment;
+    @NotBlank(message = "You have to choice type of building")
     private String buildingType;
     private Integer floor;
     private Boolean elevator;
     private Boolean balcony;
     private Boolean garage;
     private Integer yearOfConstruction;
+    @NotBlank(message = "You have to choice standard of property")
     private String standard;
 
 }
